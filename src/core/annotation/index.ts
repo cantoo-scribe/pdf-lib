f655fc98db1e1271bdf1b5482dd7a57c34fd71c0
konbraphat51 <konbraphat@gmail.com>
2025-09-05 23:02:34 +0700

    fix: export AnnotationTypes

--- src/core/annotation/index.ts
+++ src/core/annotation/index.ts
@@ -2,3 +2,4 @@ export { default as PDFAnnotation } from './PDFAnnotation';
 export { default as PDFWidgetAnnotation } from './PDFWidgetAnnotation';
 export { default as AppearanceCharacteristics } from './AppearanceCharacteristics';
 export * from './flags';
+export * from './AnnotationTypes';
